6fe230a033fa054440804729bdfc08110c0b19db
purusah <bohaichuk.v@gmail.com>
2021-11-08 20:52:33 +0200

    Pg pool config

--- src/lib/storage/index.ts
+++ src/lib/storage/index.ts
@@ -1,7 +1,12 @@
 import { Pool } from "pg";
 
 
-export const pool = new Pool();
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 export const StorageInternalError = new Error("Unpredictable Storage Error");
 export const NoRowsFoundError = new Error("No Rows Found");
