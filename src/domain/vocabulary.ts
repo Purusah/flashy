ac1ef67df000e11839e0da76294b7db7428ac9e7
purusah <bohaichuk.v@gmail.com>
2022-01-16 23:26:52 +0200

    Remove unused code

--- src/domain/vocabulary.ts
+++ src/domain/vocabulary.ts
@@ -1,68 +1,8 @@
-import { randomInt } from "node:crypto";
-
 import { ERROR_CODES, getStorage, isDatabaseError, NoRowsAffected } from "../lib/storage";
-import { Definition, DefinitionDefault, Err } from "../lib/types";
 
 const UnknownVocabularyError = new Error("unpredictable error");
 const EmptyVocabularyError = new Error("user vocabulary empty");
 const NoCurrentWordsError = new Error("current word not set");
-
-export class UserVocabulary {
-    // private userId: number;
-    private currentWord: [string, Definition] | null = null;
-    private vocabulary: Array<[string, Definition]> = [];
-
-    constructor() {
-        // this.userId = userId;
-        this.vocabulary = [];
-    }
-
-    nextWord(): [string, Err] {
-        this.currentWord = null;
-        if (this.vocabulary.length === 0) {
-            return ["", EmptyVocabularyError];
-        }
-        const word = this.vocabulary[randomInt(0, this.vocabulary.length + 1)];
-        if (word === undefined) {
-            // should exist
-            return ["", UnknownVocabularyError];
-        }
-        this.currentWord = word;
-        return [word[0], null];
-    }
-
-    nextDefinition(): [Definition, Err] {
-        this.currentWord = null;
-        if (this.vocabulary.length === 0) {
-            return [DefinitionDefault, EmptyVocabularyError];
-        }
-        const word = this.vocabulary[randomInt(0, this.vocabulary.length + 1)];
-        if (word === undefined) {
-            // should exist
-            return [DefinitionDefault, UnknownVocabularyError];
-        }
-        this.currentWord = word;
-        return [word[1], null];
-    }
-
-    revealCurrentWord(): [string, Err] {
-        if (this.currentWord !== null) {
-            return [this.currentWord[0], null];
-        }
-        return ["", NoCurrentWordsError];
-    }
-
-    revealCurrentDefinition(): [Definition, Err] {
-        if (this.currentWord !== null) {
-            return [this.currentWord[1], null];
-        }
-        return [DefinitionDefault, NoCurrentWordsError];
-    }
-
-    addWord(word: string, definition: Definition): void {
-        this.vocabulary.push([word, definition]);
-    }
-}
 
 export interface LearningPair {
     word: string;
